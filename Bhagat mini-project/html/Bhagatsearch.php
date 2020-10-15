7c3be933bb03369a31661a1fe708f81703989d8a
madan mohan <madan.mohan_cs18@gla.ac.in>
2020-10-15 23:07:35 +0530

    responsive

--- Bhagat mini-project/html/Bhagatsearch.php
+++ Bhagat mini-project/html/Bhagatsearch.php
@@ -7,14 +7,80 @@
     
     <link rel="stylesheet" href="../css/Bhagatsearch.css" >
     <link rel="stylesheet" media="screen and (max-width: 1300px)" href="../css/BhagatResponsive.css">
-   
+   <style>
+       table{
+    width: 90%;
+    margin:30px auto;
+    background-color: rgba(172, 218, 236, 0.7);
+ 
+    border-radius: 20px; 
+    
+}
+table:nth-child(even){
+    background-color: rgba(129, 119, 119, 0.7); 
+}
+table,tr,td{
+    border-collapse: collapse;
+ 
+}
+table tr{
+    margin-bottom: 30px;
+  
+    border-spacing: 20px;
+  
+}
+td{
+  padding:20px 20px 0 20px;
+  font-size: 20px;
+  color: white;
+}
+td img{
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+}
+.buy{
+width: 100px;
+border-radius: 25px;
+padding:10px;
+background-color: rgb(168, 108, 218);
+color: white;
+/* margin-right: 30px; */
+margin: 5px 30px;
+font-size: 20px;
+height: 25px;
+display: flex;
+align-items: center;
+justify-content: center;
+}
+.cn{
+width: 150px;
+border-radius: 25px;
+padding:10px;
+background-color: rgb(214, 119, 30);
+color: white;
+/* margin-right: 30px; */
+font-size: 20px;
+height: 25px;
+display: flex;
+align-items: center;
+justify-content: center;
+margin: 5px 30px;
+}
+p{
+    color: white;
+}
+.buy:hover{
+    background-color: goldenrod;
+}
+   </style>
 </head>
 <body>
     <nav id="navbar">
         <div id="logo">
         </div>
         <ul>
-            <li class="item"><a href="#home">Home</a></li>
+            <li class="item"><a href="../../index.html">Home</a></li>
             <li class="item"><a href="#c">About Us</a></li>
             <li class="item"><a href="#S">Services</a></li>
             <li class="item"><a href="#">Contact Us</a></li>
@@ -31,7 +97,7 @@
 
         <h1 class="h-primary center">SEARCH YOUR DREAM HOME</h1>
         <div id="Search-box">        
-            <form action=" ">
+            <form action="Bhagatsearch.php" method="POST">
 
                  <div class="form-group">
                      <div id="loc">
@@ -69,27 +135,117 @@
                     <div id="number">
                     
                     <label for="number">Bed-Rooms:</label>
-                    <input type="number" name="number" id="number">
+                    <input type="text" name="bhk" id="number">
                     </div>
 
-                 <label for="Bath-Rooms">Bath-Rooms:</label>
-                 <input type="number" name="number" id="Bath-Rooms">
+                 <label for="city">City:</label>
+                 <input type="text" name="city" id="city" required>
                 </div>
-            </div>
+            
 
                 <div id="button">
                 <div>
-                <input type="button" value="Search">
-                <input type="button" value="Reset">
+                <input type="submit" value="Search">
+                <input type="reset" value="Reset">
         
             </div>
+        </div>
         </form>
-         </div>
-
-        
+         </div>   
     </section>
+    <section class="search-content" >
+        
+     <?php
+     include('../../php/connection.php'); 
+     $bhk= $_POST['bhk'];  
+     $city = $_POST['city'];
+     if($bhk==""){
+         $sql="SELECT status,city,bhk,acrooms FROM house where city='$city'";
+     }
+     else{
+     $sql="SELECT status,city,bhk,acrooms FROM house where BHK='$bhk' and city='$city'";
+     }
+     $result= mysqli_query($con,$sql);
+     
+     if(mysqli_num_rows($result)){
+        while($row=mysqli_fetch_assoc($result)){
+            
+           echo "<table>";
+          
+            echo "<tr>";
+            
+              echo "<td> ";
+               
+                 echo "<h3>Specification:</h3>";  
+                 echo "<p>";
+                  echo $bhk ;
+                  echo "</p>" ;  
+                echo "<p>Address: sant nagar near pushpanjali</p>";   
+                 echo "<p>";
+                echo $city ;
+                echo ",up</p> ";
+                     
+                    echo "<p>";
+                    echo " Ac rooms";
+                    echo "</p>";
+                     echo "<h3>Available now</h3>";
+  
+                echo "</td>";
+               
+             echo "</tr>";
+         
+              
+             echo "</table>" ;
+           
+        }
+     }
+     else {
+         echo "No Result Found";
+      
+     }
+     mysqli_close($con);
+     ?>
+        
+      
+
+         <table>
+            <tr >
+                <td width="250px" rowspan="3" ><img src="../../images/h5.jpg" ></td>
+
+            
+              <tr><td style="color: rgb(86, 14, 219);">
+               
+                 <h3 style="color: black;">Specification:</h3>  
+                 <p><?php echo $bhk ?></p>   
+                <p>Address: sant nagar near pushpanjali</p>   
+                 <p> <?php echo $city ?>,up</p>
+                     
+                     <p>  Ac rooms</p>
+                     <h3 style="margin-top:10px;">Available now</h3>
+  
+                </td>
+               
+             </tr>
+         
+               <tr >
+                 <td class="cn1" style="float: left; color: black; padding:5px 5px 5px 20px;margin-top:5px;">
+                     contact no. 985632752
+                        </td>
+                   <td  class="buy" style="float: right;">
+                 <a href="LogIn.html">Buy</a>
+               </td>
+               <td class="cn" style="float: right;">
+                Rent 4000/- 
+              </td>
+             </tr>
+             
+            </tr>
+        </table> 
+    </section>
+
+    <section class="servicescontainer">
     <div id="S">
-     <section class="servicescontainer">
+     
         <h1 class="h-primary center">our services </h1>
         <div id="services">
             <div class="box">
---
+++ Bhagat mini-project/html/rn.php
@@ -0,0 +1,17 @@
+<?php
+$cl="pink";
+echo "<h1> This is ". $cl. " color </h1>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo "hi :".$cl. "kh" ;
+    ?>
+</body>
+</html>
